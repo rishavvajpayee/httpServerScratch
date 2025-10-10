41af68f47ef78bd722e3c3051bee34b3f8a267f4
rishavvajpayee <reshavvajpayee@gmail.com>
2025-10-11 01:13:58 +0530

    Added string request parser

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,11 @@
 package request
 
-import "io"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
 
 type RequestLine struct {
 	HttpVersion   string
@@ -14,16 +19,44 @@ type Request struct {
 	// Body        []byte
 }
 
-var ERROR_BAD_START_LINE error
+var SEPARATOR = "\r\n"
+var ERROR_BAD_START_LINE = fmt.Errorf("bad start line")
+
+func ParseRequestline(s string) (*RequestLine, string, error) {
+	idx := strings.Index(s, SEPARATOR)
+	if idx == -1 {
+		return nil, s, nil
+	}
+	startLine := s[:idx]
+	resOfMsg := s[idx+len(SEPARATOR):]
+
+	parts := strings.Split(startLine, " ")
+	if len(parts) != 3 {
+		return nil, resOfMsg, ERROR_BAD_START_LINE
+	}
+
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, resOfMsg, ERROR_BAD_START_LINE
+	}
+
+	rl := &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HttpVersion:   httpParts[1],
+	}
+
+	return rl, resOfMsg, nil
+}
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	requestLine := RequestLine{
-		HttpVersion:   "1.1",
-		RequestTarget: "/",
-		Method:        "GET",
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, errors.Join(fmt.Errorf("unable to io.ReadAll"), err)
 	}
-	r := Request{
-		RequestLine: requestLine,
-	}
-	return &r, nil
+	strData := string(data)
+	rl, _, err := ParseRequestline(strData)
+	return &Request{
+		RequestLine: *rl,
+	}, err
 }
