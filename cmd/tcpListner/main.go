814ced6d0479d5a5791bb13a11e62752e12d3822
rishavvajpayee <reshavvajpayee@gmail.com>
2025-10-12 09:11:25 +0530

    request line parser complete

--- cmd/tcpListner/main.go
+++ cmd/tcpListner/main.go
@@ -1,54 +1,34 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
+	"log"
 	"net"
-)
 
-func ReadLinesFromReader(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-	go func() {
-		defer f.Close()
-		defer close(out)
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				out <- str
-				data = data[i+1:]
-				str = ""
-			}
-			str += string(data)
-		}
-	}()
-	return out
-}
+	"github.com/rishavvajpayee/httpServerScratch/internal/request"
+)
 
 func main() {
 
+	fmt.Println("Rolling the Server")
 	listener, err := net.Listen("tcp", ":8000")
 	if err != nil {
 		fmt.Println("Error @Listener : ", err)
 	}
 	for {
-		fmt.Println("Rolling the Server")
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error @conn : ", err)
+			log.Fatal("Error @conn", err)
 		}
 
-		lines := ReadLinesFromReader(conn)
-		for line := range lines {
-			fmt.Printf("read : %s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("Error @RequestFromReader")
 		}
+		fmt.Printf("Request Line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 
 }
